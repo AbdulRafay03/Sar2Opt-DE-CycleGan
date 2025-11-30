fa2981b200b597143fc28ffe6bbf5316d60621b5
Abdul Rafay <shaikhabdulrafay03@gmail.com>
2025-12-01 00:29:13 +0500

    final

--- options/train_options.py
+++ options/train_options.py
@@ -28,7 +28,7 @@ class TrainOption(BaseOptions):
         parser.add_argument('--continue_train', action='store_true', help='continue training: load the latest model')
         parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
         parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
-        # training parameters 从0-50个epoch的学习率不变，从50-100学习率逐步下降
+        
         parser.add_argument('--n_epochs', type=int, default=10, help='number of epochs with the initial learning rate')
         parser.add_argument('--n_epochs_decay', type=int, default=20, help='number of epochs to linearly decay learning rate to zero')
         parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
